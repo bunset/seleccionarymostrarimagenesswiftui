768e7dc81e92132436502350bd052008c563d968
bun <bunset@gmail.com>
2024-04-16 03:42:33 +0200

    Initial Commit

---
+++ seleccionarymostrarimagenesswiftui/seleccionarymostrarimagenesswiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  seleccionarymostrarimagenesswiftuiApp.swift
+//  seleccionarymostrarimagenesswiftui
+//
+//  Created by David on 16/4/24.
+//
+
+import SwiftUI
+
+@main
+struct seleccionarymostrarimagenesswiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
